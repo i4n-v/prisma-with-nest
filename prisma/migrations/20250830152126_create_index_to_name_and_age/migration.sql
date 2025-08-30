4f12b7dc36e8b788fffca09c98d85499ae1f5cd1
i4n-v <ianvinicioss@gmail.com>
2025-08-30 14:50:34 -0300

    feat: setup database with prisma

---
+++ prisma/migrations/20250830152126_create_index_to_name_and_age/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "users_name_idx" ON "public"."users"("name");
+
+-- CreateIndex
+CREATE INDEX "users_age_idx" ON "public"."users"("age");
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from '../prisma/prisma.service';
+import { UsersModule } from '../users/users.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
